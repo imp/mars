6a1922c34f37a6ffe84a1029933e669fcb33df5f
Cyril Plisko <cyril.plisko@mountall.com>
2016-02-26 19:35:37 +0200

    Implement INDIRECT evaluation

--- src/mars/mod.rs
+++ src/mars/mod.rs
@@ -156,7 +156,7 @@ impl Core {
         result
     }
 
-    pub fn evaluate(&mut self, mode: Mode, number: Address) -> (Address, Address) {
+    pub fn evaluate(&mut self, pc: Address, mode: Mode, number: Address) -> (Address, Address) {
         // let mut rp: Address;
         // let mut wp: Address;
         let rlim = self.read_limit;
@@ -165,7 +165,15 @@ impl Core {
         match mode {
             Mode::IMMEDIATE => (0, 0),
             Mode::DIRECT => (self.fold(number, rlim), self.fold(number, wlim)),
-            Mode::INDIRECT => (self.fold(number, rlim), self.fold(number, wlim)),
+            Mode::INDIRECT => {
+                let mut rp = self.fold(number, rlim);
+                let mut wp = self.fold(number, wlim);
+                rp = self.fold((rp + self.core[((pc + rp) % self.core_size)].b_number),
+                               rlim);
+                wp = self.fold((wp + self.core[((pc + wp) % self.core_size)].b_number),
+                               wlim);
+                (rp, wp)
+            }
             Mode::DECREMENT => (self.fold(number, rlim), self.fold(number, wlim)),
             Mode::INCREMENT => (self.fold(number, rlim), self.fold(number, wlim)),
         }
